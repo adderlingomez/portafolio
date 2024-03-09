629d97f8c8c8b5f92d8f84529eb012e5c564715d
kantuspro007@outlook.com <kantuspro007@outlook.com>
2024-03-08 21:01:47 -0500

    primer commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls import include, urls
 from django.urls import path
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('',            views.inicio, name='inicio' ),
     path('portafolio/', views.portafolio, name='portafolio' ),
     path('contacto/',   views.contacto, name='contacto' ),
+    url()
 ]
